941b5b1ecbbcc375245212fc90b3533fcc32ab91
phucnguyen202 <phucnguyenrk@gmail.com>
2022-10-06 09:27:56 +0700

    phuc cpmmit

---
+++ app/src/main/java/com/example/bttuan3_nguyenvanphuc/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bttuan3_nguyenvanphuc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
